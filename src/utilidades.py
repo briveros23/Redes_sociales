d5fa17794fdaf4ca236e7d70f9dd8ddd3fde0b2e
briveros23 <briveros@unal.edu.co>
2024-02-16 20:45:51 -0500

    inicio 3.2

--- src/utilidades.py
+++ src/utilidades.py
@@ -6,4 +6,4 @@ def tallo_y_hoja(G):
     '''funcion para simular salida igraph'''
     for node in G.nodes():
         vecinos = list(G.neighbors(node))
-        print(f'{node} | {vecinos}')  
+        print(f'{node} -- {vecinos}')  
