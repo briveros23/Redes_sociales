f368c5e5e83cc6419c5dd7d9b02f4b664d0b53d6
briveros23 <briveros@unal.edu.co>
2024-02-16 20:29:33 -0500

    ejercio 1

---
+++ src/utilidades.py
@@ -0,0 +1,9 @@
+import networkx as nx
+
+
+
+def tallo_y_hoja(G):
+    '''funcion para simular salida igraph'''
+    for node in G.nodes():
+        vecinos = list(G.neighbors(node))
+        print(f'{node} | {vecinos}')  
