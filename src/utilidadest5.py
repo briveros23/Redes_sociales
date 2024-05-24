405830fd644c5d7f19fa13c963e25ba7a9d3028b
briveros23 <briveros@unal.edu.co>
2024-05-24 14:35:28 -0500

    creacion de grafos y proyecciones

--- src/utilidadest5.py
+++ src/utilidadest5.py
@@ -3,20 +3,22 @@ import igraph as ig
 
 # funcion para crear grafos bipartitos
 
-def grafo_bipartito(primer_grupo, segundo_grupo, aristas):
-    # nodos 
-    nodos = primer_grupo + segundo_grupo
-    # lista de True por ley
-    primer_tipo = [True] * len(primer_grupo)
-    # lista de false por partido politico 
-    segundo_tipo = [False] * len(segundo_grupo)
-    # sumamos ambas listas 
-    tipo = primer_tipo + segundo_tipo  
-    # creamos el grafo bipartito
-
-    conexiones_indices = [(nodos.index(nodo1), nodos.index(nodo2)) for nodo1,nodo2  in aristas]
+def grafo_biparito_t5(nodos, aristas, dato_a_evitar):
+    G = ig.Graph()
+    for nodo in nodos:
+        if nodo['id'] != dato_a_evitar:    
+            G.add_vertex(name=str(nodo['id']), label=str(nodo['label']), tipo=str(nodo['type']))
+    aristas = [ elem for elem in aristas if elem['source'] != dato_a_evitar]
+    for arista in aristas:
+        G.add_edge(str(arista['source']), str(arista['target']), weight=arista['value'])
     
-    g = ig.Graph.Bipartite(types=tipo, edges=conexiones_indices)
-    g.vs['nombre'] = nodos
+    G.vs['type'] = [False if tipo == 'author' else True for tipo in G.vs['tipo']]
+    return G
 
-    return g
+# generacion de grafos a partir de matriz de adyacencia
+
+def grafo_matriz_adyacencia_t5(matriz_adyacencia, nombres_nodos):
+    G = ig.Graph.Adjacency((matriz_adyacencia > 0).tolist(), mode = "undirected")
+    G.es['weight'] = matriz_adyacencia[matriz_adyacencia.nonzero()]
+    G.vs['label'] = nombres_nodos
+    return G
